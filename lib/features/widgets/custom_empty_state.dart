ff85d2453d42b44a292f3858f154290fb9468956
Shakirullah <omotososakiru25@gmail.com>
2026-05-07 15:32:45 +0100

    feat: extract empty state class from screen to its own class which is used across the project

---
+++ lib/features/widgets/custom_empty_state.dart
@@ -0,0 +1,51 @@
+import 'package:dose_tracker/core/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class CustomEmptyState extends StatelessWidget {
+  final String title;
+  final String description;
+  final IconData icon;
+
+  const CustomEmptyState({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.icon,
+  }); 
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              //Icons.medication_outlined,
+              size: 80,
+              color: AppColors.textHint.withValues(alpha: 0.5),
+            ),
+            const SizedBox(height: 24),
+            Text(
+              title,
+              //'No medications yet',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+                color: AppColors.textPrimary,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              description,
+             // 'Tap the + button to add your first medication',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, color: AppColors.textSecondary),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
